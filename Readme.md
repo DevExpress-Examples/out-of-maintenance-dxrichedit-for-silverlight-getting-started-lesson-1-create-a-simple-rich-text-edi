1628ac28605006720978fd92b98dedcdd3e4c1b2
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# DXRichEdit for Silverlight - Getting Started - Lesson 1 - Create a Simple Rich Text Editor
+
+
+<p>This example demonstrates how to create a simple application with the <strong>RichEditControl</strong>. It can be helpful if you are a first-time user of the <strong>DXRichEdit</strong> suite.</p><br />
+
+
+<br/>
+
+
---
+++ VB/Lesson1/MainPage.xaml.vb
@@ -0,0 +1,33 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Net
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Animation
+Imports System.Windows.Shapes
+
+Namespace Lesson1
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub NewDocumentButton_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			richEditControl1.CreateNewDocument()
+		End Sub
+
+		Private Sub SaveDocumentButton_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			richEditControl1.SaveDocumentAs()
+		End Sub
+
+		Private Sub LoadDocumentButton_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			richEditControl1.LoadDocument()
+		End Sub
+	End Class
+End Namespace
